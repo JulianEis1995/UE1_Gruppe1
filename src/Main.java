c205f4b0a2fd657a08050fd1c741739255ff3c29
Julian Eisenmann <1810653115@stud.fh-kufstein.ac.at#>
2018-10-11 15:43:10 +0200

    herkunft  hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,8 @@ public class Main
         int alter = 23;
         String vorname = "Julian";
         int jahrgang = 2018;
-        System.out.println("Hallo " + vorname + " " + nachname + "! Du bist " + alter  + " Jahre alt und kommst aus dem Jahrgang " + jahrgang + "!");
-        System.out.println(String.format("Hello %s %s! Du bist %d Jahre alt und vom Jahrgang %d!", vorname, nachname, alter, jahrgang));
+        String herkunft = "Söll";
+        System.out.println("Hallo " + vorname + " " + nachname + " aus " + herkunft + "! Du bist " + alter  + " Jahre alt und kommst aus dem Jahrgang " + jahrgang + "!");
+        System.out.println(String.format("Hello %s %s aus %s! Du bist %d Jahre alt und vom Jahrgang %d!", vorname, nachname, herkunft, alter, jahrgang));
     }
 }
