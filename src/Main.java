979a213f5e85e6b8805fa31fbff6b73db8a5181b
Julian Eisenmann <1810653115@stud.fh-kufstein.ac.at#>
2018-10-11 15:38:27 +0200

    nachname hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ public class Main
 
     public static void main(String[] args)
     {
+        String nachname = "Eisenmann";
         int alter = 23;
-        String name = "Julian";
-        System.out.println("Hello " + name + "! Du bist " + alter  + " Jahre alt");
-        System.out.println(String.format("Hello %s! Du bist %d Jahre alt.", name, alter));
+        String vorname = "Julian";
+        System.out.println("Hallo " + vorname " " + nachname + "! Du bist " + alter  + " Jahre alt");
+        System.out.println(String.format("Hello %s %s! Du bist %d Jahre alt.", vorname, nachname, alter));
     }
 }
