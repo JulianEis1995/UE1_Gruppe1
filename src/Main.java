79e9b3067160550dc0912e5749ff441e4251577a
Julian Eisenmann <1810653115@stud.fh-kufstein.ac.at#>
2018-10-11 15:41:47 +0200

    jahrgang hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main
         String nachname = "Eisenmann";
         int alter = 23;
         String vorname = "Julian";
-        System.out.println("Hallo " + vorname " " + nachname + "! Du bist " + alter  + " Jahre alt");
-        System.out.println(String.format("Hello %s %s! Du bist %d Jahre alt.", vorname, nachname, alter));
+        int jahrgang = 2018;
+        System.out.println("Hallo " + vorname + " " + nachname + "! Du bist " + alter  + " Jahre alt und kommst aus dem Jahrgang " + jahrgang + "!");
+        System.out.println(String.format("Hello %s %s! Du bist %d Jahre alt und vom Jahrgang %d!", vorname, nachname, alter, jahrgang));
     }
 }
