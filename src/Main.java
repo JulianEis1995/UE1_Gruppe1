728cd2b914ff1ffe4eb35d584711bd5bc2a24baf
Julian Eisenmann <1810653115@stud.fh-kufstein.ac.at#>
2018-10-11 15:18:06 +0200

    alter hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main
 
     public static void main(String[] args)
     {
+        int alter = 23;
         String name = "Julian";
-        System.out.println("Hello " + name + "!");
+        System.out.println("Hello " + name + "! Du bist " + alter  + " Jahre alt");
+        System.out.println(String.format("Hello %s! Du bist %d Jahre alt.", name, alter));
     }
 }
